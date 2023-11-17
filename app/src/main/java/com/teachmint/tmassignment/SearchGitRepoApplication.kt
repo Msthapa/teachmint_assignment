86781a09f434656f6a48b68621238913f9482a66
Manoj_embibe <manoj.thapa@embibe.com>
2023-11-17 22:09:07 +0530

    initialised hilt at application level

---
+++ app/src/main/java/com/teachmint/tmassignment/SearchGitRepoApplication.kt
@@ -0,0 +1,8 @@
+package com.teachmint.tmassignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SearchGitRepoApplication : Application() {
+}
