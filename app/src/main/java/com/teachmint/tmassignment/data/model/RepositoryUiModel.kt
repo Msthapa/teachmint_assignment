6c881037217f326bc4278fd51da69e093477dd6b
Manoj_embibe <manoj.thapa@embibe.com>
2023-11-19 00:46:49 +0530

    1. defined RepositoryUiModel class to show repository data into ui level

---
+++ app/src/main/java/com/teachmint/tmassignment/data/model/RepositoryUiModel.kt
@@ -0,0 +1,11 @@
+package com.teachmint.tmassignment.data.model
+
+data class RepositoryUiModel (
+    val repoId : Int,
+    val repoName : String,
+    val ownerName : String,
+    val description : String?,
+    val ownerImageUrl : String,
+    val contributors_url : String,
+    val project_link : String,
+    )
