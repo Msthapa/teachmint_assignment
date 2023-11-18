07f3380ef5b34a9d8bf09f45aeb1f4fd4b04f4f4
Manoj_embibe <manoj.thapa@embibe.com>
2023-11-18 08:51:12 +0530

    1. injecting viewModel in MainActivity to check if dagger working fine or not

--- app/src/main/java/com/teachmint/tmassignment/view/MainActivity.kt
+++ app/src/main/java/com/teachmint/tmassignment/view/MainActivity.kt
@@ -2,12 +2,17 @@ package com.teachmint.tmassignment.view
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.teachmint.tmassignment.databinding.ActivityMainBinding
+import com.teachmint.tmassignment.viewmodel.AssignmentViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding : ActivityMainBinding
+    private val mViewModel : AssignmentViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
