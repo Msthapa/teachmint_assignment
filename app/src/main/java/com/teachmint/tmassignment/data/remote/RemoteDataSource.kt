ada679a4e0a477d3c4a4d3990dce678e6881d528
Manoj_embibe <manoj.thapa@embibe.com>
2023-11-18 12:45:54 +0530

    added searchRepositories function implementation in RemoteDataSource class

--- app/src/main/java/com/teachmint/tmassignment/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/teachmint/tmassignment/data/remote/RemoteDataSource.kt
@@ -1,7 +1,14 @@
 package com.teachmint.tmassignment.data.remote
 
+import com.teachmint.tmassignment.util.BaseDataSource
 import javax.inject.Inject
 
-class RemoteDataSource @Inject constructor( private val apiService: ApiService ) {
+class RemoteDataSource @Inject constructor( private val apiService: ApiService ) : BaseDataSource() {
+
+    suspend fun searchRepositories(query : String, perPage : Int, pageNo : Int) =  handleApiCall {
+          apiService.searchRepositories(query, perPage, pageNo)
+        }
+
+
 
 }
