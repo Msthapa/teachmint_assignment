57d0c2569ba4d3a3408dc2edc512293c922378fb
Manoj_embibe <manoj.thapa@embibe.com>
2023-11-18 12:46:38 +0530

    added searchRepositories function implementation in AssignmentRepositoryImpl class, also converting server response to ui model (which can be used to display at UI level )

--- app/src/main/java/com/teachmint/tmassignment/repository/AssignmentRepositoryImpl.kt
+++ app/src/main/java/com/teachmint/tmassignment/repository/AssignmentRepositoryImpl.kt
@@ -1,12 +1,32 @@
 package com.teachmint.tmassignment.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
 import com.teachmint.tmassignment.data.local.LocalDataSource
+import com.teachmint.tmassignment.data.model.RepositoryUiModel
 import com.teachmint.tmassignment.data.remote.RemoteDataSource
+import com.teachmint.tmassignment.data.remote.model.toRepositoryItemList
+import com.teachmint.tmassignment.util.DataWrapper
 import javax.inject.Inject
 
 class AssignmentRepositoryImpl @Inject constructor(
     private val assignmentLocalDataSource: LocalDataSource,
     private val assignmentNetworkDataSource: RemoteDataSource
 ) : AssignmentRepository {
+    override suspend fun searchRepositories(query: String, perPage : Int, pageNo : Int): LiveData<DataWrapper<List<RepositoryUiModel>>> {
+        return liveData {
+            emit(DataWrapper.loading())
+            assignmentNetworkDataSource.searchRepositories(query, perPage, pageNo)?.let {
+                if (it.status == DataWrapper.Status.SUCCESS) {
+                    val uiModel = DataWrapper(DataWrapper.Status.SUCCESS,it.data?.toRepositoryItemList(), null, 200)
+                    emit(uiModel)
+                } else if (it.status == DataWrapper.Status.ERROR) {
+                    emit(DataWrapper.error(it.statusCode, it.message ?: ""))
+                }
+
+            }
+        }
+    }
+
 
 }
