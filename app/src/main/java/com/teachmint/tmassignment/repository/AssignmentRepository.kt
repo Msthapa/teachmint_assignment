ef5fb249f4ef80e0c525004b42c49bd43cd1f54f
Manoj_embibe <manoj.thapa@embibe.com>
2023-11-18 12:47:00 +0530

    added searchRepositories function in AssignmentRepository

--- app/src/main/java/com/teachmint/tmassignment/repository/AssignmentRepository.kt
+++ app/src/main/java/com/teachmint/tmassignment/repository/AssignmentRepository.kt
@@ -1,4 +1,10 @@
 package com.teachmint.tmassignment.repository
 
+import androidx.lifecycle.LiveData
+import com.teachmint.tmassignment.data.model.RepositoryUiModel
+import com.teachmint.tmassignment.util.DataWrapper
+
 interface AssignmentRepository {
+    suspend fun searchRepositories(query : String, perPage : Int, pageNo : Int) : LiveData<DataWrapper<List<RepositoryUiModel>>>
+
 }
