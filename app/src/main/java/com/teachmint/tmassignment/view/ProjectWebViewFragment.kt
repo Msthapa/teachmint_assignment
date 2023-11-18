2819f722b82d61d01aa3d22c415cdd86ef9e3bfb
Manoj_embibe <manoj.thapa@embibe.com>
2023-11-19 03:45:19 +0530

    1. added and implemented project link loading logic

--- app/src/main/java/com/teachmint/tmassignment/view/ProjectWebViewFragment.kt
+++ app/src/main/java/com/teachmint/tmassignment/view/ProjectWebViewFragment.kt
@@ -5,24 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.teachmint.tmassignment.databinding.FragmentProjectWebViewBinding
+import com.teachmint.tmassignment.util.AppConstants
 
 
 class ProjectWebViewFragment : Fragment() {
 
     lateinit var mBinding : FragmentProjectWebViewBinding
+    private var webUrl : String ?= null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         mBinding = FragmentProjectWebViewBinding.inflate(layoutInflater, container, false)
+        arguments?.let {
+            webUrl = it.getString(AppConstants.PROJECT_LINK)
+        }
         inflateUI()
         return mBinding.root
     }
 
     private fun inflateUI() {
+        webUrl?.let {
+            mBinding.wvProject.loadUrl(it)
+            mBinding.wvProject.webViewClient = object :  WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    mBinding.pbWeb.visibility = View.GONE
+                }
+            }
 
+        } ?: kotlin.run {
+            Toast.makeText(context, "render issue!", Toast.LENGTH_SHORT).show()
+            findNavController().popBackStack()
+        }
     }
 
 
