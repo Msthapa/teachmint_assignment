eb248b851f6644968497282bee827ee789b7a8b6
Manoj_embibe <manoj.thapa@embibe.com>
2023-11-18 18:09:59 +0530

    1. defined repoList adapter and viewHolder for showing repoList

---
+++ app/src/main/java/com/teachmint/tmassignment/adapters/RepoListAdapter.kt
@@ -0,0 +1,42 @@
+package com.teachmint.tmassignment.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.teachmint.tmassignment.data.model.RepositoryUiModel
+import com.teachmint.tmassignment.databinding.LayoutRepositoryItemBinding
+
+class RepoListAdapter : RecyclerView.Adapter<RepoListAdapter.RepoListItemViewHolder>() {
+
+    private var repoList = ArrayList<RepositoryUiModel>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoListItemViewHolder {
+        val binding = LayoutRepositoryItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RepoListItemViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return repoList.size
+    }
+
+    override fun onBindViewHolder(holder: RepoListItemViewHolder, position: Int) {
+       holder.bind(repoList[position])
+    }
+
+    inner class RepoListItemViewHolder(private val  binding : LayoutRepositoryItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(repositoryUiModel : RepositoryUiModel) {
+            binding.tvRepoName.text = repositoryUiModel.name
+        }
+    }
+
+    fun addRepositoryList(repoList: List<RepositoryUiModel>) {
+        this.repoList.addAll(repoList)
+        notifyDataSetChanged()
+    }
+
+    fun resetRepoList(repoList: ArrayList<RepositoryUiModel>) {
+        this.repoList = repoList
+        notifyDataSetChanged()
+    }
+}
