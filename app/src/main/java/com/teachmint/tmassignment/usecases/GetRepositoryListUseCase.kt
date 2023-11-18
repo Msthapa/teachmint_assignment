60d9be734d8c26ca7d2e5f716937a2701783003d
Manoj_embibe <manoj.thapa@embibe.com>
2023-11-18 12:44:42 +0530

    added fetch repository list method to GetRepositoryListUseCase class

--- app/src/main/java/com/teachmint/tmassignment/usecases/GetRepositoryListUseCase.kt
+++ app/src/main/java/com/teachmint/tmassignment/usecases/GetRepositoryListUseCase.kt
@@ -4,5 +4,5 @@ import com.teachmint.tmassignment.repository.AssignmentRepository
 import javax.inject.Inject
 
 class GetRepositoryListUseCase @Inject constructor(private val repository : AssignmentRepository) {
-
+    suspend fun searchRepositories(query : String, perPage : Int, pageNo : Int) = repository.searchRepositories(query, perPage, pageNo)
 }
