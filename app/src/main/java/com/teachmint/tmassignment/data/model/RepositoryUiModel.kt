a9c365571efaf9045c01c75112c53fb6b731bb74
Manoj_embibe <manoj.thapa@embibe.com>
2023-11-19 02:04:13 +0530

    1. added repository full name

--- app/src/main/java/com/teachmint/tmassignment/data/model/RepositoryUiModel.kt
+++ app/src/main/java/com/teachmint/tmassignment/data/model/RepositoryUiModel.kt
@@ -3,6 +3,7 @@ package com.teachmint.tmassignment.data.model
 data class RepositoryUiModel (
     val repoId : Int,
     val repoName : String,
+    val repoFullName : String,
     val ownerName : String,
     val description : String?,
     val ownerImageUrl : String,
